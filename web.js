7ca8943a37dddd852fd443ca9c991c4fa809f404
Andy Bauch <andy.bauch@gmail.com>
2013-07-05 00:54:50 +0000

    Update site to use index file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var indexfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var indexbuf = fs.readFileSync(indexfile);
+  response.send(indexbuf.toString());
 });
 
 var port = process.env.PORT || 5000;
